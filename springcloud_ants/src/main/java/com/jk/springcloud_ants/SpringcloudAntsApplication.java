4a275680c2610d57753b0c665c9b2d6a7a1659c4
ChqBull <1316237778@qq.com>
2019-05-20 11:39:43 +0800

    第二次提交

--- springcloud_ants/src/main/java/com/jk/springcloud_ants/SpringcloudAntsApplication.java
+++ springcloud_ants/src/main/java/com/jk/springcloud_ants/SpringcloudAntsApplication.java
@@ -3,11 +3,11 @@ package com.jk.springcloud_ants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableDiscoveryClient
+@EnableFeignClients
 public class SpringcloudAntsApplication {
 
     public static void main(String[] args) {
--- springcloud_antsam/src/main/java/com/jk/springcloud_antsam/SpringcloudAntsamApplication.java
+++ springcloud_antsam/src/main/java/com/jk/springcloud_antsam/SpringcloudAntsamApplication.java
@@ -3,11 +3,11 @@ package com.jk.springcloud_antsam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableDiscoveryClient
+@EnableFeignClients
 public class SpringcloudAntsamApplication {
 
     public static void main(String[] args) {
