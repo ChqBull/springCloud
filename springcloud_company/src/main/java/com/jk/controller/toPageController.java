09536887809349490e0cc36f4440599ed8de04b5
cmf123666 <825916511@qq.com>
2019-05-20 17:42:02 +0800

    曹沐繁提交代码

---
+++ springcloud_company/src/main/java/com/jk/controller/toPageController.java
@@ -0,0 +1,110 @@
+package com.jk.controller;
+
+import com.jk.bean.OrderBean;
+import com.jk.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class toPageController {
+     @Autowired
+     private OrderService service;
+    @RequestMapping("orderPage")
+    public String  page(Model model){
+        List<OrderBean> list1 = service.getOrdercount();
+        Long money = service.getMoney();
+        model.addAttribute("money",money);
+        model.addAttribute("count",list1.size());
+
+        return "orderPage";
+    }
+
+    @RequestMapping("todetailsPage")
+    public String todetails(Integer id,Model model){
+       OrderBean  order = service.getOrderById(id);
+       if(order.getShoulishijian()!=null && order.getShoulishijian().length()>0){
+           model.addAttribute("o",order);
+       }else{
+           order.setShoulishijian("尚未受理服务");
+           model.addAttribute("o",order);
+       }
+
+                  return "todetailsPage";
+    }
+//跳的第二个页面
+    @RequestMapping("yunshuzaitu")
+    public String  yunshuzaitu(Model model){
+        List<OrderBean> list1 = service.getOrdercount();
+        Long money = service.getMoney();
+        model.addAttribute("money",money);
+        model.addAttribute("count",list1.size());
+
+        return "yunshuzaitu";
+    }
+
+    @RequestMapping("tozaituPage")
+    public String tozaituPage(Integer id,Model model){
+        OrderBean  order = service.getOrderById(id);
+         if(order.getShoulishijian().length()>0 &&order.getShouhuoshijian().length()>0){
+             model.addAttribute("o",order);
+         }else if(order.getShoulishijian().length()>0){
+            if(order.getShouhuoshijian().length()>0){
+                model.addAttribute("o",order);
+            }else{
+             order.setShouhuoshijian("尚未选择收货");
+             model.addAttribute("o",order);
+            }
+         }else {
+
+             order.setShoulishijian("尚未选择受理服务");
+             order.setShouhuoshijian("尚未选择收货");
+             model.addAttribute("o",order);
+         }
+
+
+        return "tozaituPage";
+    }
+
+    //跳的第三个页面
+    @RequestMapping("yunsuanPage")
+    public String  yunsuanPage(Model model){
+        List<OrderBean> list1 = service.getOrdercount();
+        Long money = service.getMoney();
+        model.addAttribute("money",money);
+        model.addAttribute("count",list1.size());
+
+        return "yunsuanPage";
+    }
+
+    @RequestMapping("toyunsuanPage")
+    public String toyunsuanPage(Integer id,Model model){
+        OrderBean  order = service.getOrderById(id);
+              if(order.getShoulishijian()!=null&&order.getShoulishijian().length()>0){
+                    if(order.getShouhuoshijian()!=null&&order.getShouhuoshijian().length()>0 ){
+                         if(order.getJiesuanshijian()!=null&&order.getJiesuanshijian().length()>0){
+                             model.addAttribute("o",order);
+                         }else {
+                             order.setJiesuanshijian("用户没有结算，系统将于俩天后自动结算");
+                             model.addAttribute("o",order);
+                         }
+
+                    }else{
+                        order.setShouhuoshijian("尚未选择收货");
+                        order.setJiesuanshijian("用户没有结算，系统将于俩天后自动结算");
+                        model.addAttribute("o",order);
+                    }
+
+              }else{
+                  order.setShoulishijian("尚未选择受理");
+                  order.setShouhuoshijian("尚未选择收货");
+                  order.setJiesuanshijian("用户没有结算，系统将于俩天后自动结算");
+                  model.addAttribute("o",order);
+              }
+        return "toyunsuanPage";
+    }
+}
