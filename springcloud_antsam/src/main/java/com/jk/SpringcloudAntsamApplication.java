2dec7c587ef650ce9e7e8e768d27ad01f4de33f9
swq <1716122210@qq.com>
2019-05-21 20:30:38 +0800

    宋文倩第二次提交

---
+++ springcloud_antsam/src/main/java/com/jk/SpringcloudAntsamApplication.java
@@ -0,0 +1,17 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class SpringcloudAntsamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudAntsamApplication.class, args);
+    }
+
+}
